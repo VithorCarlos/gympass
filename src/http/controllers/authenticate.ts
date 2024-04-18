68829654e8fadd311c176159c7139b5e898d3ed6
VithorCarlos <vithor.carlos99@hotmail.com>
2024-04-18 00:21:10 -0300

    jwt

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -13,16 +13,27 @@ export async function authenticate(
   })
 
   const { email, password } = authenticateBodySchema.parse(request.body)
-
+  console.log(email)
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({ email, password })
+    const { user } = await authenticateUseCase.execute({ email, password })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+    return reply.status(200).send({
+      token,
+    })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message })
     }
     throw Error
   }
-  return reply.status(200).send()
 }
