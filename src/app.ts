80ec379a8d29724a65bd982d40376017247aaee9
VithorCarlos <vithor.carlos99@hotmail.com>
2024-05-10 23:14:34 -0300

    refresh token

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { usersRoutes } from './http/controllers/users/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
+import cookie from '@fastify/cookie'
 import { gymsRoutes } from './http/controllers/gyms/routes'
 import { checkinsRoutes } from './http/controllers/check-ins/routes'
 
@@ -10,8 +11,16 @@ export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
 
+app.register(cookie)
 app.register(usersRoutes)
 app.register(gymsRoutes)
 app.register(checkinsRoutes)
