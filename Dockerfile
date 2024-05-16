1d369b874c1a9d3cef83358363b8d9540f885763
VithorCarlos <vithor.carlos99@hotmail.com>
2024-05-15 23:54:05 -0300

    docker

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM node:18-alpine3.19
+
+WORKDIR /usr/app/gym
+
+COPY package*.json ./
+
+RUN npm cache clean --force
+
+COPY . .
+
+RUN npm install
+
+EXPOSE 3333
+
+RUN npx prisma migrate reset
+
+RUN npx prisma generate
+
+RUN npm run build && npm run lint && npm run test 
+
+
+CMD [ "npm", "run", "start" ]
+
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import { app } from '@/app'
-import { env } from '@/env'
+import { app } from "@/app";
+import { env } from "@/env";
 
-app.listen({ host: '0.0.0.0', port: env.PORT }).then(() => {
-  console.log('HTTP Server Runnig! 🚀')
-})
+app.listen({ host: "0.0.0.0", port: env.PORT }).then(() => {
+  console.log("HTTP Server Runnig! 🚀, at port:" + env.PORT);
+});
